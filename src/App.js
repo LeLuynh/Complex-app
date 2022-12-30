d376ef0452dca2cb6d69d135df9cd025fcc87f42
lptluynh <lptluynh@tma.com.vn>
2022-12-30 13:55:32 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './main.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeGuest from './components/HomeGuest';
+import About from './components/About';
+import Term from './components/Term';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='main'>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={< HomeGuest />}></Route>
+          <Route exact path='/about-us' element={< About />}></Route>
+          <Route exact path='/terms' element={< Term />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
